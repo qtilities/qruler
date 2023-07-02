08f798868d65508cea5bb642370826b60406ac6a
redtide <redtid3@gmail.com>
2023-07-02 19:45:22 +0200

    Refactor
    - added Italian locale translation (desktop and application)
    - added a local CMake module to automate resources management
    - compressed icon
    - changed Appstream id

---
+++ cmake/QtAppResources.cmake
@@ -0,0 +1,68 @@
+# Installs:
+# - translation files via lxqt-build-tools
+# - application' desktop file
+# - application' icon
+# - appstream metainfo
+
+# LXQt build tools modules
+if (UNIX AND NOT APPLE)
+    include(GNUInstallDirs)
+    set(LXQT_CMAKE_MODULES_DIR "/usr/share/cmake/lxqt-build-tools/modules")
+    list(APPEND CMAKE_MODULE_PATH ${LXQT_CMAKE_MODULES_DIR})
+else()
+    # TODO: Windows and macOS
+endif()
+
+# Translations
+option(UPDATE_TRANSLATIONS "Update source translation files" OFF)
+
+include(LXQtPreventInSourceBuilds)
+include(LXQtTranslateDesktop)
+include(LXQtTranslateTs)
+include(LXQtTranslationLoader)
+
+if (UNIX AND NOT APPLE)
+    set(APPDATA_FILE_IN
+        "resources/linux/${PROJECT_APPSTREAM_ID}.appdata.xml.in"
+    )
+    set(APPDATA_FILE_OUT
+        "${PROJECT_APPSTREAM_ID}.appdata.xml"
+    )
+    set(DESKTOP_FILE_IN
+        resources/linux/${PROJECT_APPSTREAM_ID}.desktop.in
+    )
+    # TODO
+    # - Assign a description.html file content to PROJECT_DESCRIPTION variable
+    # - Git tags array from function to PROJECT_RELEASES variable
+
+    configure_file(
+        ${APPDATA_FILE_IN}
+        ${APPDATA_FILE_OUT}
+    )
+endif()
+# TRANSLATION_DIR     "${CMAKE_CURRENT_SOURCE_DIR}/resources/translations"
+lxqt_translate_ts(QM_FILES
+    UPDATE_TRANSLATIONS ${UPDATE_TRANSLATIONS}
+    SOURCES             ${PROJECT_FILES}
+    TRANSLATION_DIR     "resources/translations"
+    INSTALL_DIR         "${CMAKE_INSTALL_FULL_DATADIR}/${PROJECT_ID}/translations"
+)
+lxqt_translate_desktop(DESKTOP_FILES
+    SOURCES             "${CMAKE_CURRENT_SOURCE_DIR}/${DESKTOP_FILE_IN}"
+    TRANSLATION_DIR     "resources/translations"
+    USE_YAML
+)
+lxqt_app_translation_loader(SOURCES ${PROJECT_NAME})
+
+# Other resources
+if (UNIX AND NOT APPLE)
+    install(FILES "${CMAKE_CURRENT_SOURCE_DIR}/resources/icons/${PROJECT_ID}.svgz"
+        DESTINATION "${CMAKE_INSTALL_FULL_DATADIR}/icons/hicolor/scalable/apps"
+    )
+    install(FILES "${DESKTOP_FILES}"
+        DESTINATION "${CMAKE_INSTALL_FULL_DATADIR}/applications"
+    )
+    install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${APPDATA_FILE_OUT}"
+        DESTINATION "${CMAKE_INSTALL_FULL_DATADIR}/metainfo"
+    )
+endif()
---
+++ resources/translations/qruler.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>DialogPrefs</name>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="43"/>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="58"/>
+        <source>Opacity:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="65"/>
+        <source>Always on top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="90"/>
+        <source>Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="105"/>
+        <source>Background</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="139"/>
+        <source>Border</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="146"/>
+        <source>Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>azd::DialogPrefs</name>
+    <message>
+        <location filename="../../src/dialogprefs.cpp" line="43"/>
+        <source>Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>azd::QRuler</name>
+    <message>
+        <location filename="../../src/qruler.cpp" line="40"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/qruler.cpp" line="150"/>
+        <source>Width:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/qruler.cpp" line="150"/>
+        <source>px</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/qruler.cpp" line="151"/>
+        <source>Height:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/qruler.cpp" line="214"/>
+        <source>Error loading about file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/qruler.cpp" line="220"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
