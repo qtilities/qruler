21be8b28045a72fbd6b12ac4341add25ecce85d2
stefonarch <standreas@riseup.net>
2023-07-05 23:04:00 +0200

    Retake original qruler_it.ts

--- resources/translations/qruler_it.ts
+++ resources/translations/qruler_it.ts
@@ -2,114 +2,66 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="it">
 <context>
-    <name>DialogAbout</name>
-    <message>
-        <location filename="../../src/dialogabout.ui" line="27"/>
-        <source>Informations</source>
-        <translation>Informazioni</translation>
-    </message>
-    <message>
-        <location filename="../../src/dialogabout.ui" line="62"/>
-        <source>qrc:/about.html</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../src/dialogabout.ui" line="81"/>
-        <source>Thanks</source>
-        <translation>Ringraziamenti</translation>
-    </message>
-    <message>
-        <location filename="../../src/dialogabout.ui" line="119"/>
-        <source>qrc:/thanks.html</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../src/dialogabout.ui" line="138"/>
-        <source>License</source>
-        <translation>Licenza</translation>
-    </message>
-    <message>
-        <location filename="../../src/dialogabout.ui" line="176"/>
-        <source>qrc:/license.html</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
     <name>DialogPrefs</name>
     <message>
-        <location filename="../../src/dialogprefs.ui" line="49"/>
         <source>General</source>
-        <translation>Generali</translation>
+        <translation>Generale</translation>
     </message>
     <message>
-        <location filename="../../src/dialogprefs.ui" line="64"/>
-        <source>Opacity:</source>
-        <translation>Opacità</translation>
-    </message>
-    <message>
-        <location filename="../../src/dialogprefs.ui" line="87"/>
         <source>Always on top</source>
-        <translation>Sempre al di sopra</translation>
+        <translation>Sempre in primo piano</translation>
     </message>
     <message>
-        <location filename="../../src/dialogprefs.ui" line="96"/>
         <source>Colors</source>
         <translation>Colori</translation>
     </message>
     <message>
-        <location filename="../../src/dialogprefs.ui" line="111"/>
         <source>Background</source>
         <translation>Sfondo</translation>
     </message>
     <message>
-        <location filename="../../src/dialogprefs.ui" line="145"/>
         <source>Border</source>
         <translation>Bordo</translation>
     </message>
     <message>
-        <location filename="../../src/dialogprefs.ui" line="152"/>
-        <source>Text</source>
+        <source>Foreground</source>
         <translation>Testo</translation>
+    </message>
+    <message>
+        <source>Opacity:</source>
+        <translation>Opacità:</translation>
     </message>
 </context>
 <context>
-    <name>QRuler::DialogPrefs</name>
+    <name>azd::DialogPrefs</name>
     <message>
-        <location filename="../../src/dialogprefs.cpp" line="51"/>
         <source>Preferences</source>
         <translation>Preferenze</translation>
     </message>
 </context>
 <context>
-    <name>QRuler::MainWindow</name>
+    <name>azd::QRuler</name>
     <message>
-        <location filename="../../src/mainwindow.cpp" line="51"/>
-        <source>&amp;Preferences</source>
-        <translation>&amp;Preferenze</translation>
+        <source>Quit</source>
+        <translation>Esci</translation>
     </message>
     <message>
-        <location filename="../../src/mainwindow.cpp" line="54"/>
-        <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <source>Error loading about file</source>
+        <translation>Errore cariacamento file di informazioni</translation>
     </message>
     <message>
-        <location filename="../../src/mainwindow.cpp" line="56"/>
-        <source>&amp;Quit</source>
-        <translation>&amp;Esci</translation>
+        <source>About</source>
+        <translation>Informazioni</translation>
     </message>
     <message>
-        <location filename="../../src/mainwindow.cpp" line="148"/>
         <source>Width:</source>
         <translation>Larghezza:</translation>
     </message>
     <message>
-        <location filename="../../src/mainwindow.cpp" line="148"/>
-        <location filename="../../src/mainwindow.cpp" line="149"/>
         <source>px</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/mainwindow.cpp" line="149"/>
         <source>Height:</source>
         <translation>Altezza:</translation>
     </message>
