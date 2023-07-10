f7ca464b2702411cfb0764eca9e8e102bef112e0
redtide <redtid3@gmail.com>
2023-07-10 02:40:03 +0200

    CMake: added qrulerConfig.cmake

--- cmake/QtAppResources.cmake
+++ cmake/QtAppResources.cmake
@@ -1,86 +1,78 @@
-# Installs:
-# - translation files via lxqt-build-tools
-# - application' desktop file
-# - application' icon
-# - appstream metainfo
-
-# LXQt build tools modules
+#===============================================================================
+# QtAppResources.cmake
+#
+# Configure and Installs:
+# - About information markdown file
+# - Appstream metainfo
+# - Desktop file
+# - Translation files via lxqt-build-tools
+#
+#   TODO: Windows and macOS
+#===============================================================================
+# Configure files
+#===============================================================================
 if (UNIX AND NOT APPLE)
     include(GNUInstallDirs)
-    # TODO: Search in PATH
-    set(LXQT_CMAKE_MODULES_DIR "/usr/share/cmake/lxqt-build-tools/modules")
-    list(APPEND CMAKE_MODULE_PATH ${LXQT_CMAKE_MODULES_DIR})
-else()
-    # TODO: Windows and macOS
-endif()
-
-# Translations
-option(UPDATE_TRANSLATIONS "Update source translation files" OFF)
-
-include(LXQtPreventInSourceBuilds)
-include(LXQtTranslateDesktop)
-include(LXQtTranslateTs)
-include(LXQtTranslationLoader)
-
-
-if (UNIX AND NOT APPLE)
-    set(BUILD_DATADIR "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_INSTALL_DATADIR}")
-    set(BUILD_BINDIR "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_INSTALL_BINDIR}")
-    # TODO :Assign a "description.html file" content to PROJECT_DESCRIPTION variable
+    # "resources/about.md" is a gitignored file configured by cmake and then added
+    # to the Qt resource file, so not needed in the build directory.
+    configure_file("resources/in/about.info.md.in"
+        "${CMAKE_CURRENT_SOURCE_DIR}/resources/about.md" @ONLY
+    )
+    # TODO :Create a "description.html" file from PROJECT_DESCRIPTION
+    #       And insert its content into "PROJECT_APPDATA_FILE", see
     # (https://freedesktop.org/software/appstream/docs/chap-Metadata.html#tag-description)
     # and a git tags string list from function to PROJECT_RELEASES variable
     # (https://freedesktop.org/software/appstream/docs/chap-Metadata.html#tag-releases)
-    set(APPICON_FILE_SRC "resources/icons/application.icon.${PROJECT_ICON_FORMAT}")
-    set(APPDATA_FILE_IN  "resources/in/linux/application.appdata.xml.in")
-    set(DESKTOP_FILE_IN  "resources/in/linux/application.desktop.in")
-
-    set(APPICON_FILE_NAME "${PROJECT_APPSTREAM_ID}.${PROJECT_ICON_FORMAT}")
-    set(APPICON_FILE_BUILD_PATH "${BUILD_DATADIR}/icons/hicolor/scalable/apps/${APPICON_FILE_NAME}")
-    set(APPDATA_FILE_BUILD_PATH "${BUILD_DATADIR}/metainfo/${PROJECT_APPSTREAM_ID}.appdata.xml")
+    set(PROJECT_APPDATA_FILE_NAME "${PROJECT_APPSTREAM_ID}.appdata.xml")
+    configure_file("resources/in/linux/application.appdata.xml.in"
+        "${PROJECT_APPDATA_FILE_NAME}" @ONLY
+    )
     # TODO: This file needs to be processed first by `configure_file()` for `@` variables,
     # then the resulting file, the one below, to be processed by `lxqt_translate_desktop`.
     # Would be better doing this in one step in `lxqt_translate_desktop` only.
-    set(DESKTOP_FILE_BUILD_PATH "${BUILD_DATADIR}/applications/${PROJECT_APPSTREAM_ID}.desktop.in")
+    set(DESKTOP_FILE_IN "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_APPSTREAM_ID}.desktop.in")
+    configure_file("resources/in/linux/application.desktop.in"
+        "${DESKTOP_FILE_IN}" @ONLY
+    )
+    set(PROJECT_ICON_FILE_NAME "${PROJECT_APPSTREAM_ID}.${PROJECT_ICON_FORMAT}")
+    set(PROJECT_ICON_FILE_PATH "${CMAKE_INSTALL_FULL_DATADIR}/icons/hicolor/scalable/apps")
+    configure_file("resources/icons/application.icon.${PROJECT_ICON_FORMAT}"
+        "${CMAKE_BINARY_DIR}/${PROJECT_ICON_FILE_NAME}" COPYONLY @ONLY
+    )
+    #===========================================================================
+    # Translations
+    #===========================================================================
+    # TODO: Search in PATH
+    set(LXQT_CMAKE_MODULES_DIR "/usr/share/cmake/lxqt-build-tools/modules")
+    list(APPEND CMAKE_MODULE_PATH ${LXQT_CMAKE_MODULES_DIR})
 
-    message(STATUS "APPDATA_FILE_BUILD_PATH:  ${APPDATA_FILE_BUILD_PATH}")
-    message(STATUS "APPICON_FILE_BUILD_PATH:  ${APPICON_FILE_BUILD_PATH}")
-    message(STATUS "DESKTOP_FILE_BUILD_PATH:  ${DESKTOP_FILE_BUILD_PATH}")
+    option(UPDATE_TRANSLATIONS "Update source translation files" OFF)
 
-    configure_file(${APPDATA_FILE_IN}  "${APPDATA_FILE_BUILD_PATH}" @ONLY)
-    configure_file(${APPICON_FILE_SRC} "${APPICON_FILE_BUILD_PATH}" COPYONLY @ONLY)
-    configure_file(${DESKTOP_FILE_IN}  "${DESKTOP_FILE_BUILD_PATH}" @ONLY)
-endif()
+    # TODO: include(LXQtPreventInSourceBuilds) ???
+    include(LXQtTranslateDesktop)
+    include(LXQtTranslateTs)
 
-# APPINFO_FILE_OUT is a gitignored file configured by cmake and then added
-# to the Qt resource file, so not needed in build directory
-set(APPINFO_FILE_IN  "resources/in/about.info.md.in")
-set(APPINFO_FILE_OUT "${CMAKE_CURRENT_SOURCE_DIR}/resources/about.md")
-configure_file(${APPINFO_FILE_IN} ${APPINFO_FILE_OUT} @ONLY)
-
-# TRANSLATION_DIR     "${CMAKE_CURRENT_SOURCE_DIR}/resources/translations"
-lxqt_translate_ts(PROJECT_QM_FILES
-    UPDATE_TRANSLATIONS ${UPDATE_TRANSLATIONS}
-    SOURCES             ${PROJECT_SOURCES}
-    TRANSLATION_DIR     "resources/translations"
-    INSTALL_DIR         "${CMAKE_INSTALL_DATADIR}/${PROJECT_ID}/translations"
-)
-lxqt_translate_desktop(PROJECT_DESKTOP_FILE
-    SOURCES             "${DESKTOP_FILE_BUILD_PATH}"
-    TRANSLATION_DIR     "resources/translations"
-    USE_YAML
-)
-lxqt_app_translation_loader(SOURCES ${PROJECT_NAME})
-
-# Other resources
-if (UNIX AND NOT APPLE)
-    install(FILES "${APPDATA_FILE_BUILD_PATH}"
+    lxqt_translate_ts(PROJECT_QM_FILES
+        UPDATE_TRANSLATIONS ${UPDATE_TRANSLATIONS}
+        SOURCES             ${PROJECT_SOURCES} ${PROJECT_UI_FILES}
+        TRANSLATION_DIR     "resources/translations"
+        INSTALL_DIR         "${CMAKE_INSTALL_DATADIR}/${PROJECT_ID}/translations"
+    )
+    lxqt_translate_desktop(PROJECT_DESKTOP_FILES
+        SOURCES             "${DESKTOP_FILE_IN}"
+        TRANSLATION_DIR     "resources/translations"
+        USE_YAML
+    )
+#===============================================================================
+# Install
+#===============================================================================
+    install(FILES "${PROJECT_APPDATA_FILE_NAME}"
         DESTINATION "${CMAKE_INSTALL_DATADIR}/metainfo"
     )
-    install(FILES "${APPICON_FILE_BUILD_PATH}"
-        DESTINATION "${CMAKE_INSTALL_DATADIR}/icons/hicolor/scalable/apps"
-        RENAME "${APPICON_FILE_NAME}"
-    )
-    install(FILES "${PROJECT_DESKTOP_FILE}"
+    install(FILES "${PROJECT_DESKTOP_FILES}"
         DESTINATION "${CMAKE_INSTALL_DATADIR}/applications"
     )
 endif()
+install(FILES "${PROJECT_ICON_FILE_NAME}"
+    DESTINATION "${PROJECT_ICON_FILE_PATH}"
+)
---
+++ qrulerConfig.cmake
@@ -0,0 +1,27 @@
+#===============================================================================
+# Editable project configuration
+#
+# Essential, non translatable application information (except DESCRIPTION).
+# translatable strings are passed via code.
+#===============================================================================
+string(TOLOWER ${PROJECT_NAME} PROJECT_ID)   # Might not be compatible with AppStream
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/cmake)
+list(APPEND PROJECT_CATEGORIES "Qt;Utility") # Freedesktop menu categories
+set(PROJECT_AUTHOR_NAME        "redtide")
+set(PROJECT_AUTHOR_EMAIL       "redtid3@gmail.com") # Used also for organization email
+set(PROJECT_COPYRIGHT_YEAR     "2021-2023") # TODO: from git
+set(PROJECT_DESCRIPTION        "Simple ruler tool in Qt")
+set(PROJECT_HOMEPAGE_URL       "http://qtilities.github.io/qruler")
+set(PROJECT_ICON_FORMAT        "svg")       # TODO: macOS and Windows
+set(PROJECT_ORGANIZATION_NAME  "qtilities") # Might be equal to PROJECT_AUTHOR_NAME
+set(PROJECT_ORGANIZATION_URL   "${PROJECT_ORGANIZATION_NAME}.github.io")
+set(PROJECT_REPOSITORY_URL     "https://github.com/${PROJECT_ORGANIZATION_NAME}/${PROJECT_ID}")
+set(PROJECT_REPOSITORY_BRANCH  "master")
+set(PROJECT_SPDX_ID            "GPL-3.0-only")
+#===============================================================================
+# Appstream
+#===============================================================================
+set(PROJECT_APPSTREAM_SPDX_ID  "CC0-1.0")
+include(AppStream)
+to_appstream_id("io.github.${PROJECT_ORGANIZATION_NAME}.${PROJECT_ID}"
+    PROJECT_APPSTREAM_ID)
