8ef7fe04f5dd210ad99f632e72e62ac8ff5d793f
Baptiste Huchon <baptiste.huchon@ikmail.com>
2023-07-11 21:05:41 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (22 of 22 strings)
    
    Translation: Qtilities/QRuler
    Translate-URL: https://translate.lxqt-project.org/projects/redtide/qruler/fr/

--- resources/translations/qruler_fr.ts
+++ resources/translations/qruler_fr.ts
@@ -6,42 +6,42 @@
     <message>
         <location filename="../../src/dialogabout.cpp" line="46"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>À propos</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.cpp" line="54"/>
         <source>Author</source>
-        <translation type="unfinished"></translation>
+        <translation>Auteur</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="27"/>
         <source>Informations</source>
-        <translation type="unfinished"></translation>
+        <translation>Informations</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="62"/>
         <source>qrc:/about.html</source>
-        <translation type="unfinished"></translation>
+        <translation>qrc:/about.html</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="81"/>
         <source>Thanks</source>
-        <translation type="unfinished"></translation>
+        <translation>Remerciements</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="119"/>
         <source>qrc:/thanks.html</source>
-        <translation type="unfinished"></translation>
+        <translation>qrc:/thanks.html</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="138"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation>Licence</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="176"/>
         <source>qrc:/license.html</source>
-        <translation type="unfinished"></translation>
+        <translation>qrc:/license.html</translation>
     </message>
 </context>
 <context>
@@ -49,42 +49,42 @@
     <message>
         <location filename="../../src/dialogprefs.cpp" line="52"/>
         <source>Preferences</source>
-        <translation type="unfinished"></translation>
+        <translation>Préférences</translation>
     </message>
     <message>
         <location filename="../../src/dialogprefs.ui" line="21"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Général</translation>
     </message>
     <message>
         <location filename="../../src/dialogprefs.ui" line="52"/>
         <source>Opacity:</source>
-        <translation type="unfinished"></translation>
+        <translation>Opacité :</translation>
     </message>
     <message>
         <location filename="../../src/dialogprefs.ui" line="59"/>
         <source>Always on top</source>
-        <translation type="unfinished"></translation>
+        <translation>Toujours au dessus</translation>
     </message>
     <message>
         <location filename="../../src/dialogprefs.ui" line="81"/>
         <source>Colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Couleurs</translation>
     </message>
     <message>
         <location filename="../../src/dialogprefs.ui" line="124"/>
         <source>Border</source>
-        <translation type="unfinished"></translation>
+        <translation>Bordure</translation>
     </message>
     <message>
         <location filename="../../src/dialogprefs.ui" line="110"/>
         <source>Background</source>
-        <translation type="unfinished"></translation>
+        <translation>Arrière plan</translation>
     </message>
     <message>
         <location filename="../../src/dialogprefs.ui" line="96"/>
         <source>Text</source>
-        <translation type="unfinished"></translation>
+        <translation>Texte</translation>
     </message>
 </context>
 <context>
@@ -92,33 +92,33 @@
     <message>
         <location filename="../../src/mainwindow.cpp" line="53"/>
         <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;A propos</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="54"/>
         <source>&amp;Preferences</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Préférences</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="58"/>
         <source>&amp;Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Quitter</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="144"/>
         <source>Width:</source>
-        <translation type="unfinished"></translation>
+        <translation>Largeur :</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="144"/>
         <location filename="../../src/mainwindow.cpp" line="145"/>
         <source>px</source>
-        <translation type="unfinished"></translation>
+        <translation>px</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="145"/>
         <source>Height:</source>
-        <translation type="unfinished"></translation>
+        <translation>Hauteur :</translation>
     </message>
 </context>
 </TS>
