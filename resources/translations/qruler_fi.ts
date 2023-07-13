7ac4f305b45afb9b807e9cf20ec5aa601392fcc1
Jouni Järvinen <jounijarvis@gmail.com>
2023-07-13 00:18:16 +0000

    Translated using Weblate (Finnish)
    Currently translated at 90.9% (20 of 22 strings)
    
    Translation: Qtilities/QRuler
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/qruler/fi/

--- resources/translations/qruler_fi.ts
+++ resources/translations/qruler_fi.ts
@@ -6,42 +6,42 @@
     <message>
         <location filename="../../src/dialogabout.cpp" line="46"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Tietoja</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.cpp" line="54"/>
         <source>Author</source>
-        <translation type="unfinished"></translation>
+        <translation>Tekijä</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="27"/>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiedot</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="62"/>
         <source>qrc:/about.html</source>
-        <translation type="unfinished"></translation>
+        <translation>qrc:/about.html</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="81"/>
         <source>Thanks</source>
-        <translation type="unfinished"></translation>
+        <translation>Kiitokset</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="119"/>
         <source>qrc:/thanks.html</source>
-        <translation type="unfinished"></translation>
+        <translation>qrc:/thanks.html</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="138"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation>Lisenssi</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="176"/>
         <source>qrc:/license.html</source>
-        <translation type="unfinished"></translation>
+        <translation>qrc:/license.html</translation>
     </message>
 </context>
 <context>
@@ -49,27 +49,27 @@
     <message>
         <location filename="../../src/dialogprefs.cpp" line="52"/>
         <source>Preferences</source>
-        <translation type="unfinished"></translation>
+        <translation>Asetukset</translation>
     </message>
     <message>
         <location filename="../../src/dialogprefs.ui" line="21"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Yleiset</translation>
     </message>
     <message>
         <location filename="../../src/dialogprefs.ui" line="52"/>
         <source>Opacity:</source>
-        <translation type="unfinished"></translation>
+        <translation>Peittävyys:</translation>
     </message>
     <message>
         <location filename="../../src/dialogprefs.ui" line="59"/>
         <source>Always on top</source>
-        <translation type="unfinished"></translation>
+        <translation>Aina ylimpänä</translation>
     </message>
     <message>
         <location filename="../../src/dialogprefs.ui" line="81"/>
         <source>Colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Värit</translation>
     </message>
     <message>
         <location filename="../../src/dialogprefs.ui" line="124"/>
@@ -79,12 +79,12 @@
     <message>
         <location filename="../../src/dialogprefs.ui" line="110"/>
         <source>Background</source>
-        <translation type="unfinished"></translation>
+        <translation>Tausta</translation>
     </message>
     <message>
         <location filename="../../src/dialogprefs.ui" line="96"/>
         <source>Text</source>
-        <translation type="unfinished"></translation>
+        <translation>Teksti</translation>
     </message>
 </context>
 <context>
@@ -92,33 +92,33 @@
     <message>
         <location filename="../../src/mainwindow.cpp" line="53"/>
         <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Tietoja</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="54"/>
         <source>&amp;Preferences</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Asetukset</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="58"/>
         <source>&amp;Quit</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&amp;Lopeta</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="144"/>
         <source>Width:</source>
-        <translation type="unfinished"></translation>
+        <translation>Leveys:</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="144"/>
         <location filename="../../src/mainwindow.cpp" line="145"/>
         <source>px</source>
-        <translation type="unfinished"></translation>
+        <translation>px</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.cpp" line="145"/>
         <source>Height:</source>
-        <translation type="unfinished"></translation>
+        <translation>Korkeus:</translation>
     </message>
 </context>
 </TS>
