97eb08e3a0be7e949a740e0c9802acb5fa51a04e
Weblate <noreply@weblate.org>
2024-03-12 00:23:32 +0100

    Weblate commits
    Co-authored-by: A S Alam <amanpreet.alam@gmail.com>
    Co-authored-by: Ettore Atalan <atalanttore@googlemail.com>
    Co-authored-by: Jan Rolski <wbcwknvstb@proton.me>
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>
    Co-authored-by: Sabri Ünal <libreajans@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/qruler-menu-entry/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/qruler-menu-entry/pl/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/qruler-menu-entry/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/qruler/de/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/qruler/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/qruler/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/qruler/pl/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/qruler/tr/
    Translation: Qtilities/QRuler
    Translation: Qtilities/QRuler Menu Entry

--- resources/translations/qruler_fi.ts
+++ resources/translations/qruler_fi.ts
@@ -26,7 +26,7 @@
     <message>
         <location filename="../../src/dialogabout.ui" line="27"/>
         <source>Information</source>
-        <translation>Tietoja</translation>
+        <translation type="unfinished">Tiedot</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="62"/>
