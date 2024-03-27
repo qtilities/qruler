adce469510efa85241d6482aa596965aaf6c3877
Weblate <noreply@weblate.org>
2024-03-27 23:23:35 +0100

    Weblate commits
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/qruler-menu-entry/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/qruler/pt/
    Translation: Qtilities/QRuler
    Translation: Qtilities/QRuler Menu Entry

---
+++ resources/translations/qruler_pt.ts
@@ -0,0 +1,129 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt">
+<context>
+    <name>Qtilities::Application</name>
+    <message>
+        <location filename="../../src/application.cpp" line="102"/>
+        <source>&amp;About</source>
+        <translation>&amp;Sobre</translation>
+    </message>
+    <message>
+        <location filename="../../src/application.cpp" line="106"/>
+        <location filename="../../src/application.cpp" line="111"/>
+        <source>&amp;Preferences</source>
+        <translation>&amp;Preferências</translation>
+    </message>
+    <message>
+        <location filename="../../src/application.cpp" line="107"/>
+        <location filename="../../src/application.cpp" line="112"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Sair</translation>
+    </message>
+</context>
+<context>
+    <name>Qtilities::DialogAbout</name>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="27"/>
+        <source>Information</source>
+        <translation>Informação</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="62"/>
+        <source>qrc:/about.html</source>
+        <translation>qrc:/about.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="81"/>
+        <source>Thanks</source>
+        <translation>Obrigado</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="119"/>
+        <source>qrc:/thanks.html</source>
+        <translation>qrc:/thanks.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="138"/>
+        <source>License</source>
+        <translation>Licença</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="176"/>
+        <source>qrc:/license.html</source>
+        <translation>qrc:/license.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.cpp" line="55"/>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.cpp" line="60"/>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+</context>
+<context>
+    <name>Qtilities::DialogPrefs</name>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="36"/>
+        <source>General</source>
+        <translation>Geral</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="70"/>
+        <source>Opacity:</source>
+        <translation>Opacidade:</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="77"/>
+        <source>Always on top</source>
+        <translation>Sempre no topo</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="99"/>
+        <source>Colors</source>
+        <translation>Cores</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="120"/>
+        <source>Background</source>
+        <translation>Fundo</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="143"/>
+        <source>Border</source>
+        <translation>Margem</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="160"/>
+        <source>Text</source>
+        <translation>Texto</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.cpp" line="41"/>
+        <source>Preferences</source>
+        <translation>Preferências</translation>
+    </message>
+</context>
+<context>
+    <name>Qtilities::MainWindow</name>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="106"/>
+        <source>Width:</source>
+        <translation>Largura:</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="106"/>
+        <location filename="../../src/mainwindow.cpp" line="107"/>
+        <source>px</source>
+        <translation>px</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="106"/>
+        <source>Height:</source>
+        <translation>Altura:</translation>
+    </message>
+</context>
+</TS>
